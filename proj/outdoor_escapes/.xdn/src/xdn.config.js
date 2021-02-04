47d8be46ddcfde3f3789014d3ccf2a6e16397cbf
Ivan Liao <ivanhliao@gmail.com>
2021-02-04 11:09:15 -0500

    2021/02/04 11:00

--- proj/outdoor_escapes/.xdn/src/xdn.config.js
+++ proj/outdoor_escapes/.xdn/src/xdn.config.js
@@ -1,3 +1,11 @@
 // This file was automatically added by xdn deploy.
 // You should commit this file to source control.
 module.exports = {}
+
+
+total jobs
+	filtering job posting date
+	freshness
+feature analysis
+filter presets
+analysis design perspective
