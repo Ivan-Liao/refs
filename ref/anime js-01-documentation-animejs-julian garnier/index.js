f84aa79ff2cab35d0379199802bfb4addd27d921
Ivan Liao <ivanhliao@gmail.com>
2021-02-04 12:08:01 -0500

    2021/02/04 12:07

---
+++ ref/anime js-01-documentation-animejs-julian garnier/index.js
@@ -0,0 +1,9 @@
+anime({
+  targets: '.square',
+  scale: [
+    {value: .1, easing: 'easeOutSine', duration: 500},
+    {value: 1, easing: 'easeInOutQuad', duration: 1200}
+  ],
+  delay: anime.stagger(200, {grid: [1, 4], from: 'center'}),
+  loop: true,
+});
